fbbb2aabe9142ddeefae01c79fb55bda009a1be7
khoilvgcc210040 <khoilieuct03@gmail.com>
2025-06-26 14:22:03 +0700

    finish ci/cd

--- env-to-cloudrun-vars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ENV_FILE=${1:-.env}
-if [ ! -f "$ENV_FILE" ]; then
-  echo "❌ Không tìm thấy file $ENV_FILE"
-  exit 1
-fi
-ENV_VARS=$(grep -v '^#' "$ENV_FILE" | grep -v '^\s*$' | sed 's/"/\\"/g' | paste -sd, -)
-echo "$ENV_VARS"
